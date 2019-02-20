ab39cdc6c4f258e4680e48c70ed7488d6a7a5114
ryo matsui <r4537mbel@gmail.com>
2019-02-20 19:30:22 +0900

    configuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-        registrations: 'registrations'
-      }
+  devise_for :users,
+    path: "",
+    path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' },
+    controllers: { registrations: 'registrations' }
+
   root to: "home#index"
   get  '/about',   to: 'home#about'
 
