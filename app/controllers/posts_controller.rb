eb94a4c16bc7c08e4b187e1b6330552b409414c9
ryo matsui <r4537mbel@gmail.com>
2019-02-26 19:09:26 +0900

    create posts/edit template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     if params[:user_id]
@@ -20,14 +21,54 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = current_user.posts.find(params[:id])
   end
 
   def create
   end
 
   def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post, notice: "投稿内容を更新しました。"
+    else
+      render 'edit'
+    end
   end
+
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.update_attributes(user_params)
+  #     flash[:success] = "Profile updated"
+  #     redirect_to @user
+  #   else
+  #     render 'edit'
+  #   end
+  # end
+  #
+  # def update
+  #   @post = current_user.entries.find(params[:id])
+  #   @entry.assign_attributes(entry_params)
+  #   if @entry.save
+  #     redirect_to @entry, notice: "記事を更新しました。"
+  #   else
+  #     render "edit"
+  #   end
+  # end
 
   def destroy
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:name, :description, :price,
+                                   :purchased, :purchased_at, :published)
+    end
+
+    def correct_user
+      post = Post.find(params[:id])
+      user = post.user
+      redirect_to(root_url) unless user == current_user
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
-
-  validates :name,  presence: true, length: { maximum: 200 }
-  validates :posted_at,  presence: true
+  validates :name, presence: true, length: { maximum: 200 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+  validates :posted_at, presence: true
+  validates :purchased, :published, inclusion: { in: [true, false] }
 
   #公開ポスト
   scope :published, -> { where(published: true) }
@@ -12,9 +13,6 @@ class Post < ApplicationRecord
   #scope :user_post, ->(user) { where("user_id = ?", user.id) }
   #任意のユーザーがいた場合は、そのユーザーのポストと公開ポスト、いなければ公開ポスト
   scope :readable_for, ->(user) { user ? full(user) : published }
-
-  # scope :full, ->(member) { where("member_id = ? OR status <> ?", member.id, "draft") }
-  # scope :readable_for, ->(member) { member ? full(member) : common }
 
   scope :purchased, -> { where(purchased: true) }
   scope :unpurchased, -> { where(purchased: false) }
