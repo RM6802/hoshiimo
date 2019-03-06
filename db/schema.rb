7a25b3f086c8f82add4017ca93983b5d274c3ad4
ryo matsui <r4537mbel@gmail.com>
2019-03-06 23:13:37 +0900

    add confirmation function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_152549) do
+ActiveRecord::Schema.define(version: 2019_03_06_140320) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 2019_02_27_152549) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.text "bio"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- test/mailers/previews/devise_preview.rb
+++ test/mailers/previews/devise_preview.rb
@@ -2,4 +2,8 @@ class DevisePreview < ActionMailer::Preview
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(User.last, Devise.friendly_token)
   end
+
+  def confirmation_instructions
+    Devise::Mailer.confirmation_instructions(User.new, Devise.friendly_token)
+  end
 end
