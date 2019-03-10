a1176287fba6b31b7efb6105229d157c4f4aa5db
ryo matsui <r4537mbel@gmail.com>
2019-03-10 17:02:39 +0900

    fix templates and scss

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, length: { maximum: 50 }
-  validates :description, length: { maximum: 200 }
+  validates :description, length: { maximum: 500 }
   validates :user_id, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
   validates :purchased, :published, inclusion: { in: [true, false] }
